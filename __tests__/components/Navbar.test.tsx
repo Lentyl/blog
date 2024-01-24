be96a1c3a6cf197c262ee1f8be4504154736853b
Mariusz Podgorski <m.b.podgorski1984@gmail.com>
2024-01-24 06:00:34 +0100

    setaup .env in githubActions

--- __tests__/components/Navbar.test.tsx
+++ __tests__/components/Navbar.test.tsx
@@ -1,14 +1,13 @@
 import { render, screen } from '@testing-library/react'
 import Navbar from '@/components/nav/Navbar'
 import '@testing-library/jest-dom'
-import { loadEnvConfig } from '@next/env'
 
 
 
 describe('Navbar', () => {
 
   beforeEach(() => {
-    loadEnvConfig(process.cwd())
+
   });
 
   it('should have Navbar text', () => {
